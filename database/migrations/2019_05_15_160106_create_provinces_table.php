0fd2def04c31494d5c4aec96d3bfda5a212d6fd0
Dương Mạnh Hoàng <manhhoang3151996@gmail.com>
2019-05-16 13:26:25 +0700

    init migration & models

--- database/migrations/2019_05_15_160106_create_provinces_table.php
+++ database/migrations/2019_05_15_160106_create_provinces_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreatePasswordResetsTable extends Migration
+class CreateProvincesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,11 @@ class CreatePasswordResetsTable extends Migration
      */
     public function up()
     {
-        Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email')->index();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('provinces', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->unique();
+            $table->integer('regions')->comment('1: miền bắc|2: miền trung|3: miền nam');
+            $table->timestamps();
         });
     }
 
@@ -27,6 +28,6 @@ class CreatePasswordResetsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('password_resets');
+        Schema::dropIfExists('provinces');
     }
 }
